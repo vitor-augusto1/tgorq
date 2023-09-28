5b124b3dacfceb56f03fadf7f9d9073724b3f256
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-09-27 21:01:09 -0300

    feat: Assign the Update method for the main model

--- main.go
+++ main.go
@@ -23,3 +23,16 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	m.url, cmd = m.url.Update(msg)
+	return m, cmd
+}
+
