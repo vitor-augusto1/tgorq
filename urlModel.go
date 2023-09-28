f46a6be67723d72b0b834391f587778f19f034d7
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-09-28 19:23:51 -0300

    feat: Create a new function to initialize the Url method as a textinput

--- urlModel.go
+++ urlModel.go
@@ -11,3 +11,16 @@ type Url struct {
    style lipgloss.Style
 }
 
+func InitialUrlModel() *Url {
+  ti := textinput.New()
+  ti.Placeholder = "https://www.example.com/"
+  ti.Focus()
+  return &Url {
+    url: ti,
+    style: lipgloss.NewStyle().
+                    BorderForeground(lipgloss.Color("36")).
+                    BorderStyle(lipgloss.NormalBorder()).
+                    Padding(1).Width(80),
+  }
+}
+
