fa01afc243a800cb842cdabd5a427eb94961cedb
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-09-28 19:23:23 -0300

    feat: Create a new Url model

---
+++ urlModel.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Url struct {
+   url textinput.Model
+   style lipgloss.Style
+}
+
