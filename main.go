2e817a801867af26ea7f8446b14d3607902fc92a
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-09-27 21:00:21 -0300

    feat: Set up the main model

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+type model struct {
+	url        textinput.Model
+	method     string
+  request    Request
+  response   Response
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "https://www.example.com/"
+	ti.Focus()
+  return model{
+    url: ti,
+    method: "GET",
+    request: InitialRequestModel(),
+    response: InitalResponseModel(),
+  }
+}
+
