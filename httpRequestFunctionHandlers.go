ec51e533383e9eb1316bc53f9c2b4b6923139ab1
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-10-16 20:57:39 -0300

    feat: Create a new GET request and save the response.body and response.headers on the `Response` model

--- httpRequestFunctionHandlers.go
+++ httpRequestFunctionHandlers.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
 
 func (m mainModel) handleGetMethod(url string) {
-  res, err := http.Get(url)
-  if err != nil {
-    m.response.body.SetValue(err.Error())
-  }
-  // Receive the bytes from the request
-  body, err := io.ReadAll(res.Body)
+  var req *http.Request
+  var resp *http.Response
+  var responseBody []byte
+  var err error
+
+  // Initialize new request
+  req, err = http.NewRequest(GET.String(), url, nil)
   if err != nil {
     m.response.body.SetValue(err.Error())
   }
 
-  defer res.Body.Close()
+  // Make the request
+  resp, err = http.DefaultClient.Do(req)
+  if err != nil {
+    m.response.body.SetValue(err.Error())
+  }
 
-  // Convert the bytes to string
-  toStringBody := string(body)
-  m.response.body.SetValue(toStringBody)
-  m.response.headers.SetValue(string(body)
+  // Read the bytes from the response body
+  responseBody, err = io.ReadAll(resp.Body)
+  if err != nil {
+    m.response.body.SetValue(err.Error())
+  }
+
+  var stringToBeStoreInTheResponseHeaderTextArea string
+  for k, v := range resp.Header {
+    stringToBeStoreInTheResponseHeaderTextArea += fmt.Sprintf(
+      "%q : %q\n", k, v,
+    )
+  }
+
+  // Set response body and headers
+  m.response.body.SetValue(string(responseBody))
+  m.response.headers.SetValue(stringToBeStoreInTheResponseHeaderTextArea)
+
+  defer resp.Body.Close()
 }
