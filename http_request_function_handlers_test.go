14f07ff4d1686b51e78425929a4f24992ef58fb3
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-10-24 14:01:00 -0300

    feat: Create new function to test the POST method handler function

--- http_request_function_handlers_test.go
+++ http_request_function_handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -8,7 +9,7 @@ import (
 )
 
 func TestHandleGetMethod(t *testing.T) {
-  expectedResponse := "{'data': 'dummy'}"
+  expectedResponse := `{"data": "dummy"}`
 
   nServer := httptest.NewServer(
     http.HandlerFunc(
@@ -20,6 +21,43 @@ func TestHandleGetMethod(t *testing.T) {
   defer nServer.Close()
 
   resp, _ := handleGetMethod(nServer.URL)
+  if resp.rawResponse != expectedResponse {
+    t.Errorf("Expected response to be %s got %s", expectedResponse, resp.rawResponse)
+  }
+}
+
+func TestHandlePostMethod(t *testing.T) {
+  expectedResponse := `{"success": "id 101 created"}`
+  bodyString := `
+  {
+    "id": 101,
+    "title": "foo",
+    "body": "bar",
+    "userId": 1
+  }
+  `
+  headerString := `{"Content-type": "application/json; charset=UTF-8"}`
+  byteBody := bytes.NewBuffer([]byte(bodyString))
+  byteHeaders := []byte(headerString)
+
+  nServer := httptest.NewServer(
+    http.HandlerFunc(
+      func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodPost {
+          t.Errorf("Expected a POST request, got %s", r.Method)
+          return 
+        }
+        fmt.Fprintf(w, expectedResponse)
+      },
+    ),
+  )
+  defer nServer.Close()
+
+  resp, err := handlePostMethod(nServer.URL, byteBody, byteHeaders)
+  if err != nil {
+    t.Errorf("POST request failed: %s", err)
+    return 
+  }
   if resp.rawResponse != expectedResponse {
     t.Errorf("Expected response to be %s got %s", expectedResponse, resp.rawResponse)
   }
