e79f77290b4ef35b59f7aaa65cd66ac833623c79
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-10-24 16:45:54 -0300

    feat: Create new `mainModel` method to store the current app state in `~/.config/tgorq/state.json` file

--- save_state_handler.go
+++ save_state_handler.go
@@ -35,3 +35,38 @@ func fileExists(path string) bool {
   return err == nil
 }
 
+func (m mainModel) storeCurrentState() {
+  configDir, err := os.UserConfigDir()
+  if err != nil {
+    log.Println("Error finding the config dir: ", err)
+    return
+  }
+
+	filePath := filepath.Join(configDir, "tgorq")
+	savedStateFile := filepath.Join(configDir, "tgorq", "state.json")
+
+  // Check if the path exists. If not, create it.
+  if !fileExists(filePath) {
+    err := os.Mkdir(filePath, 0755)
+    if err != nil {
+      log.Println("Error making directory: ", err)
+      return
+    }
+  }
+
+  // Checking if the file exists and create it if not
+  if !fileExists(savedStateFile) {
+    f, err := os.Create(savedStateFile)
+    if err != nil {
+      log.Println("Error creating the state file: ", err)
+      return
+    }
+    defer f.Close()
+  }
+
+  f, err := os.OpenFile(savedStateFile, os.O_SYNC|os.O_CREATE|os.O_WRONLY, 0644)
+  if err != nil {
+   log.Println(err)
+  }
+  writeToFile(f, "test")
+}
