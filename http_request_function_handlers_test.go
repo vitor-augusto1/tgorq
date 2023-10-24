a554b40f37a2fc5506fefc9f0d4c3325e4b89120
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-10-24 14:01:20 -0300

    feat: Create new function to test the PUT method handler function

--- http_request_function_handlers_test.go
+++ http_request_function_handlers_test.go
@@ -63,3 +63,41 @@ func TestHandlePostMethod(t *testing.T) {
   }
 }
 
+func TestHandlePutMethod(t *testing.T) {
+  expectedResponse := `{"success": "101 updated successfully"}`
+  bodyString := `
+  {
+    "id": 101,
+    "title": "bar",
+    "body": "foo",
+    "userId": 1
+  }
+  `
+  headerString := `{"Content-type": "application/json; charset=UTF-8"}`
+  byteBody := bytes.NewBuffer([]byte(bodyString))
+  byteHeaders := []byte(headerString)
+
+  nServer := httptest.NewServer(
+    http.HandlerFunc(
+      func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodPut {
+          t.Errorf("Expected a PUT request, got %s", r.Method)
+          return 
+        }
+        fmt.Fprintf(w, expectedResponse)
+      },
+    ),
+  )
+  defer nServer.Close()
+
+  resp, err := handlePutMethod(nServer.URL, byteBody, byteHeaders)
+  if err != nil {
+    t.Errorf("PUT request failed: %s", err)
+    return 
+  }
+  if resp.rawResponse != expectedResponse {
+    t.Errorf("Expected response to be %s got %s", expectedResponse, resp.rawResponse)
+  }
+}
+
+
